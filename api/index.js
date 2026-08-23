c768b2910eb20307e3bf39fe431a95b75caaae35
Tayyaba Amin <tayyabaamin780@gmail.com>
2026-08-23 15:13:15 +0500

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import connectDB from './db/database.js';
 import userRoutes from './routes/user.route.js';
 import authRouter from "./routes/auth.route.js"
+import listingRouter from './routes/listing.route.js'
 import errorMiddleware from './middlewares/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRouter)
+app.use('/api/listing', listingRouter)
 
 app.use(errorMiddleware);
 

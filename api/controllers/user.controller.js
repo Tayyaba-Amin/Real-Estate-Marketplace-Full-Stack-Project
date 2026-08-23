b973227e5d262e63d4f698ba264c2a08c948cbfe
Tayyaba Amin <tayyabaamin780@gmail.com>
2026-08-23 13:11:17 +0500

    complete update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -10,19 +10,18 @@ export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id) {
         return next(errorHandler(401, "You are not allowed to update this user!"));
     }
-
     try {
         if (req.body.password) {
             req.body.password = await bcrypt.hash(req.body.password, 10);
         }
-        const updatedUser=await User.findByIdAndUpdate(req.params.id,{
-             $set:{
-             username: req.body.username,
-             email: req.body.email,
-             password: req.body.password
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password
             }
-        },{new:true})
-        const { password, ...others } = updatedUser._doc; 
+        }, { new: true })
+        const { password, ...others } = updatedUser._doc;
         res.status(200).json(others);
     } catch (error) {
         next(error);
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,52 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function Profile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <img
           src={currentUser.avatar}
           alt="Profile"
@@ -14,29 +55,41 @@ export default function Profile() {
         <input
           type="text"
           placeholder="username"
+          defaultValue={currentUser.username}
           className="border  border-slate-300 bg-slate-50 p-3 text-slate-800 outline-none rounded-lg "
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
+          defaultValue={currentUser.email}
           className="border  border-slate-300 bg-slate-50 p-3 text-slate-800 outline-none rounded-lg "
           id="email"
+          onChange={handleChange}
         />
         <input
-          type="text"
+          type="password"
           placeholder="password"
           className="border  border-slate-300 bg-slate-50 p-3 text-slate-800 outline-none rounded-lg "
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Updating..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess ? "Profile updated successfully!" : ""}
+      </p>
     </div>
   );
 }

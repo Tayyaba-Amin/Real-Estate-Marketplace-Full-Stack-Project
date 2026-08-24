2d9c4ab22e48871bc2cb854f4d8380643513ff43
Tayyaba Amin <tayyabaamin780@gmail.com>
2026-08-24 21:37:02 +0500

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ import authRouter from "./routes/auth.route.js"
 import listingRouter from './routes/listing.route.js'
 import errorMiddleware from './middlewares/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
 connectDB();
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -17,6 +19,12 @@ app.use(cookieParser());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('/{*splat}', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use(errorMiddleware);
 

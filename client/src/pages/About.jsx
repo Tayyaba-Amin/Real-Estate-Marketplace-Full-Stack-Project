392ce27709839615fd5e5bafa91111a8c339030d
Tayyaba Amin <tayyabaamin780@gmail.com>
2026-08-24 20:25:25 +0500

    create about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,30 @@
-import React from "react";
-
 export default function About() {
-  return <div className="text-red-400">About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl  text-slate-800 font-bold mb-4">
+        About PropertyHub
+      </h1>
+      <p className="mb-4 text-slate-700">
+        PropertyHub is a modern real estate platform designed to make finding,
+        buying, selling, and renting properties simple and convenient. Whether
+        you're looking for your dream home, a comfortable apartment, or a
+        valuable investment opportunity, PropertyHub brings a wide range of
+        property listings together in one easy-to-use platform.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our goal is to connect property seekers with suitable properties while
+        providing a smooth and reliable experience. Users can search and filter
+        listings based on property type, price, location, parking, furnishing,
+        and special offers, making it easier to find a property that matches
+        their needs and budget.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At PropertyHub, we believe finding the right property should be
+        straightforward and stress-free. With detailed property information,
+        high-quality images, useful search features, and direct communication
+        with property owners, PropertyHub helps turn your property search into a
+        simple and confident journey.
+      </p>
+    </div>
+  );
 }
